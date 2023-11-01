0b420a6024fbd388fc68ac1821afdfb06f428f82
DnKylDs <delossantosdeankyle26@gmail.com>
2023-11-01 22:38:40 +0800

    Update delete.php

--- functions/delete.php
+++ functions/delete.php
@@ -1,6 +1,6 @@
 <?php
     include_once("../dbConnection/mysqlconfig_connection.php");
     $id = $_GET['id'];
-    mysqli_query($dbc, "DELETE from tblsubject where subject_id='$id'");
+    mysqli_query($dbc, "delete from tblsubject where subject_id='$id'");
     header("Location:../index.php");
 ?>
