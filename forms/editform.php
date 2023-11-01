c25fc341417b661599a82b32b98f345fad8d8eee
DnKylDs <delossantosdeankyle26@gmail.com>
2023-11-01 22:19:01 +0800

    Update editform.php

--- forms/editform.php
+++ forms/editform.php
@@ -7,3 +7,15 @@
         $name = $res['subject_name'];
     }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Data</title>
+    </head>
+    <body>
+        <h1>Edit Subject</h1>
+        <a href ="../index.php">Home</a>
+        <br/><br/>
+        <form name="form1" method="post" action="../functions/edit.php>
+    </body>
+</html>
