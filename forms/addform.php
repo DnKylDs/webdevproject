86ef283edd26ed84e3f3dd8a7c5344e78b3fd67e
DnKylDs <delossantosdeankyle26@gmail.com>
2023-11-01 21:28:35 +0800

    Update addform.php

---
+++ forms/addform.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Data</title>
+</head>
+<body>
+    <h1>Add Subject</h1>
+    <a href="../index.php">Home</a>
+    <br/><br/>
+    <form action="../functions/add.php" method="post" name="form1">
+</body>
+
+</html>
